2c9f377265a76f60119c1f2a472f578940133af4
Prashant Timalsina <119124486+Prashant432142@users.noreply.github.com>
2023-04-12 20:08:21 +0545

    Add files via upload

---
+++ w-6 workshop.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WEEK 6 workshop</title>
+    </head>
+    <body>
+         <?php
+         echo "each question can be accessed by uncommenting code sections";
+            // 5 to 15 print
+            // for ($i=5;$i<=15;$i++){
+            //     print($i);
+            // }
+
+            //print hello world
+            // $a="hello world";
+            // echo $a;
+
+            //Check Student grade
+            // $a=readline('Enter a string: ');
+            // if($a>=90){echo "A+";}
+            // elseif ($a>=80) {echo "A" ;}
+            // elseif ($a>=60){echo"B";}
+            // elseif($a>=40){echo "C";}
+            // else{
+            //     echo "FAIL!!!";
+            // }
+
+            //Factorial
+            // $a=5;
+            // $n=1;
+            // if($a>1){
+            // for ($i=2;$i<=$a;$i++){
+            //     $n*=$i;
+            // }}
+            // echo $n;
+
+            //create * wala loop pattern
+            // $n=8;
+            // for($i=0;$i<$n;$i++){
+            //     for ($j=0;$j<$i;$j++){
+            //         echo"*";
+            //     }
+            // }
+
+            //multidimensional array
+            // $list = array( 
+            //     "Italy"=>"Rome", 
+            //     "Luxembourg"=>"Luxembourg", 
+            //     "Belgium"=> "Brussels", 
+            //     "Denmark"=>"Copenhagen", 
+            //     "Finland"=>"Helsinki", 
+            //     "France" => "Paris", 
+            //     "Slovakia"=>"Bratislava", 
+            //     "Slovenia"=>"Ljubljana", 
+            //     "Germany" => "Berlin", 
+            //     "Greece" => "Athens", 
+            //     "Ireland"=>"Dublin", 
+            //     "Netherlands"=>"Amsterdam", 
+            //     "Portugal"=>"Lisbon", 
+            //     "Spain"=>"Madrid", 
+            //     "Sweden"=>"Stockholm", 
+            //     "United Kingdom"=>"London", 
+            //     "Cyprus"=>"Nicosia", 
+            //     "Lithuania"=>"Vilnius", 
+            //     "Czech Republic"=>"Prague", 
+            //     "Estonia"=>"Tallin", 
+            //     "Hungary"=>"Budapest", 
+            //     "Latvia"=>"Riga", 
+            //     "Malta"=>"Valetta", 
+            //     "Austria" => "Vienna", 
+            //     "Poland"=>"Warsaw") ;
+            //     echo asort($list);
+
+            //palindrome
+            //  $a="madam";
+            //  function palin($a){
+            //     $b=strrev($a);
+            //     if ($b===$a){
+            //         echo "palindrome";
+            //     }else{
+            //         echo "not palindrome";
+            //     }
+            //  }
+            //  palin($a);
+
+            //variable swap
+            // $a=19;
+            // $b=21;
+            // echo "Numbers before swapping:\n";
+            // echo "a= ".$a." b= ".$b;
+            // $temp=$a;
+            // $a=$b;
+            // $b=$temp;
+            // echo "\nNumbers before swapping:\n";
+            // echo "a= ".$a." b= ".$b;
+
+            //print multiples
+            // define("a",5);
+            // for ($i=1;$i<=10;$i++){
+            //     $mult=$i*a;
+            //     echo $mult;
+            //     echo "<br>";
+            // }
+            
+            //armstrong number
+            // $number=153;
+            // while ($number!=0){
+            //     $x=$number%10;
+            //     $sum+=$x**3;
+            //     $number/=10;
+            // }
+            // if($number==$sum){
+            //     echo $number." is a armstrong number";
+            // }
+            // else{
+            //     echo $number." is not a armstrong number";
+            // }
+            // echo $sum;
+
+        ?> 
+
+    </body>
+</html>
